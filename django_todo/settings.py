9c57906a0096104afca5ad904696acc5a47a2692
23052015 <stani.ste13@gmail.com>
2023-03-18 09:15:33 +0000

    fix bug in settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['stanisavljevic-django-todo-app.herokuapp.com/']
+ALLOWED_HOSTS = ['stanisavljevic-django-todo-app.herokuapp.com']
 
 
 # Application definition
